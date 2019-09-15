09ac4c62311642c9973c40be3d8221208096329c
mubarak23 <mubarakaminu340@gmail.com>
2019-09-15 17:06:05 +0100

    create auth middleware and login controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,6 +16,52 @@ exports.signup = (req, res, next) => {
                         message: 'Single User created for admin access'
                     });
                 }
+            ).then(
+                (error) =>{
+                  return res.status(400).json({
+                      error
+                  });  
+                }
+            );
+        }
+    );
+}
+
+exports.login = (req, res, next) =>{
+    User.findOne({
+        email: req.body.email
+    }).then(
+        (user) =>{
+            //check user
+            if(!user){
+                return res.status(400).json({
+                    message: 'User does not exits'
+                });
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if(!valid){
+                        return res.status(400).json({
+                            message: 'Incorrect password provided'
+                        });
+                    }
+                    //sign the token
+                    const token = jwt.sign(
+                        {userId: user.id},
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h'}
+                    )
+                    return res.status(200).json({
+                        userId: user._id,
+                        token
+                    });
+                }
+            ).catch(
+                (error) =>{
+                    return res.status(400).json({
+                        error
+                    });
+                }   
             );
         }
     );
